258c16040343e3af42ad3d0d8cdd1e7ebd397252
Abhi <abhinav118@gmail.com>
2025-08-19 16:31:10 -0400

    Add components/AddTaskButton.tsx

---
+++ components/AddTaskButton.tsx
@@ -0,0 +1,13 @@
+interface AddTaskButtonProps {
+  variant?: any
+  size?: any
+  icon?: any
+}
+
+export default function AddTaskButton({ variant, size, icon }: AddTaskButtonProps) {
+  return (
+    <div className="">
+      {/* AddTaskButton component content */}
+    </div>
+  )
+}
