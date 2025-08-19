35eec9f1a23f7fc48c8512ee23fa8a129892e7eb
Abhi <abhinav118@gmail.com>
2025-08-19 16:31:11 -0400

    Add components/Sidebar.tsx

---
+++ components/Sidebar.tsx
@@ -0,0 +1,12 @@
+interface SidebarProps {
+  collapsible?: any
+  width?: any
+}
+
+export default function Sidebar({ collapsible, width }: SidebarProps) {
+  return (
+    <div className="">
+      {/* Sidebar component content */}
+    </div>
+  )
+}
