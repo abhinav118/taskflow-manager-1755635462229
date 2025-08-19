479897daec77cfc08890aced07f4c0dac21bc622
Abhi <abhinav118@gmail.com>
2025-08-19 16:31:10 -0400

    Add components/TaskModal.tsx

---
+++ components/TaskModal.tsx
@@ -0,0 +1,12 @@
+interface TaskModalProps {
+  size?: any
+  closable?: any
+}
+
+export default function TaskModal({ size, closable }: TaskModalProps) {
+  return (
+    <div className="">
+      {/* TaskModal component content */}
+    </div>
+  )
+}
